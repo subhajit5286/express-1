65a3b0d89aa37437ccf6e263b3999bb35b55bb8e
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2023-04-05 22:39:33 -0700

    body parser express 2

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 //const http=require('http');
 const express=require('express');
+const bodyParser=require('body-parser')
+
 const app=express();
-app.use((req,res,next)=>{
-    console.log('In the middleware')
-    next()
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type"submit">Add Product</button></form>') 
 })
-app.use((req,res,next)=>{
-    console.log('In Another middleware')
-    res.send('<h1> hello to node js </h1>') 
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+   
+})
+app.use('/',(req,res,next)=>{
+    res.send('Welcome to Express') 
    
 })
 //const server=http.createServer(app);
