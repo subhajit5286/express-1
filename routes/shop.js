b232e818c375533102c0ba86f3975bd064fe9be5
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2023-04-05 23:44:41 -0700

    Routers and Filters express 3

---
+++ routes/shop.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router = express.Router()
+router.get('/',(req,res,next)=>{
+    res.send('Welcome to Express') 
+   
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 //const http=require('http');
 const express=require('express');
 const bodyParser=require('body-parser')
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
 
 const app=express();
-
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type"submit">Add Product</button></form>') 
-})
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-   
-})
-app.use('/',(req,res,next)=>{
-    res.send('Welcome to Express') 
-   
+
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
 //const server=http.createServer(app);
 app.listen(3000);
